1b4ec55abfee99cfc338549786e265143a1f1375
zhelebovsk <qrabeq@gmail.com>
2020-08-18 13:16:21 +0300

    color

--- main.py
+++ main.py
@@ -6,25 +6,25 @@ b = 1.0                              # хорда
 gamma = 60.0                         # угол установки
 beta = np.array([45.0, 30.0])        # угол входа и выхода (по скоростям)
 n = 100                              # количество сегментов лопатки
-r = np.array([0.08, 0.045])           # абсолютные значения радиуса вх и вых кромки
-x0 = 0                               # нулевая координата
-y0 = 0
+r = np.array([0.08, 0.045])          # абсолютные значения радиуса вх и вых кромки
+x0 = 0.0                             # нулевая координата
+y0 = 0.0
 angin = 30.0                         # угол входной кромки
-angout = 15.0                        # угол выходной кромки
-t = 1.2                              # абсолютный шаг
+angout = 10.0                        # угол выходной кромки
+t = 1.0                              # абсолютный шаг
 
 # центральная линия по углам потока
 B = curve.curve(x0, y0, b, beta, gamma, n)
 
 # координаты начала и конца профиля лопатки
-spvh = np.array([B[0, 0]+r[0]*np.cos(np.pi/2 - np.deg2rad(beta[0] - angin / 2.0)),
-                 B[0, 1]-r[0]*np.sin(np.pi/2 - np.deg2rad(beta[0] - angin / 2.0))])
-spvih = np.array([B[-1, 0]+r[1]*np.cos(np.pi/2 - np.deg2rad(beta[1] - angout / 2.0)),
-                  B[-1, 1]+r[1]*np.sin(np.pi/2 - np.deg2rad(beta[1] - angout / 2.0))])
-corvh = np.array([B[0, 0]-r[0]*np.cos(np.pi/2 - np.deg2rad(beta[0] + angin / 2.0)),
-                  B[0, 1]+r[0]*np.sin(np.pi/2 - np.deg2rad(beta[0] + angin / 2.0))])
-corvih = np.array([B[-1, 0]-r[1]*np.cos(np.pi/2 - np.deg2rad(beta[1] + angout / 2.0)),
-                   B[-1, 1]-r[1]*np.sin(np.pi/2 - np.deg2rad(beta[1] + angout / 2.0))])
+spvh = np.array([B[0, 0] + r[0] * np.cos(np.pi / 2.0 - np.deg2rad(beta[0] - angin / 2.0)),
+                 B[0, 1] - r[0] * np.sin(np.pi / 2.0 - np.deg2rad(beta[0] - angin / 2.0))])
+spvih = np.array([B[-1, 0] + r[1] * np.cos(np.pi / 2.0 - np.deg2rad(beta[1] - angout / 2.0)),
+                  B[-1, 1] + r[1] * np.sin(np.pi / 2.0 - np.deg2rad(beta[1] - angout / 2.0))])
+corvh = np.array([B[0, 0] - r[0] * np.cos(np.pi / 2.0 - np.deg2rad(beta[0] + angin / 2.0)),
+                  B[0, 1] + r[0]*np.sin(np.pi / 2.0 - np.deg2rad(beta[0] + angin / 2.0))])
+corvih = np.array([B[-1, 0] - r[1] * np.cos(np.pi / 2.0 - np.deg2rad(beta[1] + angout / 2.0)),
+                   B[-1, 1] - r[1] * np.sin(np.pi / 2.0 - np.deg2rad(beta[1] + angout / 2.0))])
 
 # условный угол установки и длина профиля
 angsp, distsp = curve.angndist(spvh, spvih)
@@ -45,22 +45,24 @@ fig, ax = plt.subplots()
 #plt.plot(corvh[0], corvh[1], 'ro')
 #plt.plot(spvih[0],spvih[1],'bo')
 #plt.plot(corvih[0],corvih[1],'ro')
-circle1 = plt.Circle((B[0, 0], B[0, 1]), r[0])
-circle2 = plt.Circle((B[-1, 0], B[-1, 1]), r[1])
+circle1 = plt.Circle((B[0, 0], B[0, 1]), r[0], color='r', fill=False)
+circle2 = plt.Circle((B[-1, 0], B[-1, 1]), r[1], color='r', fill=False)
 ax.add_artist(circle1)
 ax.add_artist(circle2)
-plt.plot(A[:, 0], A[:, 1])
-plt.plot(B[:, 0], B[:, 1])
-plt.plot(C[:, 0], C[:, 1])
+plt.plot(A[:, 0], A[:, 1], 'r')
+plt.plot(B[:, 0], B[:, 1], 'b-.')
+plt.plot(C[:, 0], C[:, 1], 'r')
 
 # вторая лопатка
-circle3 = plt.Circle((B[0, 0] + t, B[0, 1]), r[0])
-circle4 = plt.Circle((B[-1, 0] + t, B[-1, 1]), r[1])
+circle3 = plt.Circle((B[0, 0] + t, B[0, 1]), r[0], color='r', fill=False)
+circle4 = plt.Circle((B[-1, 0] + t, B[-1, 1]), r[1], color='r', fill=False)
 ax.add_artist(circle3)
 ax.add_artist(circle4)
-plt.plot(A[:, 0] + t, A[:, 1])
-plt.plot(C[:, 0] + t, C[:, 1])
+plt.plot(A[:, 0] + t, A[:, 1], 'r')
+plt.plot(B[:, 0] + t, B[:, 1], 'b-.')
+plt.plot(C[:, 0] + t, C[:, 1], 'r')
 # настройка
 plt.axis('equal')
 plt.gca().invert_yaxis()
 plt.show()
+breakpoint()
